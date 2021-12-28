40c420105e58cdfa179b4192deabf56644d0bb85
AiramLemes <airam.lemes101@alu.ulpgc.es>
2021-12-28 13:25:56 +0000

    Image Viewer. Práctica en Aula IS2

---
+++ src/main/java/imageviewer/Main.java
@@ -0,0 +1,21 @@
+
+package imageviewer;
+
+import imageviewer.controller.MainFrame;
+import imageviewer.model.Image;
+import imageviewer.persistence.FileImageLoader;
+import java.io.File;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        File file = new File("../ImageViewer/images/");
+        
+        FileImageLoader loader = new FileImageLoader(file);
+        Image image = loader.load();
+        
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.getImageDisplay().show(image);
+    }
+}
---
+++ src/main/java/imageviewer/ui/ImageDisplay.java
@@ -0,0 +1,14 @@
+
+package imageviewer.ui;
+
+import imageviewer.model.Image;
+
+
+public interface ImageDisplay {
+    
+    Image current();
+    void show(Image image);
+    
+    
+    
+}
