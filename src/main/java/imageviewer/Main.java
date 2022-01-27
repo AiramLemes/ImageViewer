ed1d007c2d00427fae9ecbd9d54dfd4b9021b513
AiramLemes <90828450+AiramLemes@users.noreply.github.com>
2022-01-27 10:14:03 +0000

    Update Main.java
    Se cambia la ruta donde están almacenadas las fotos

--- src/main/java/imageviewer/Main.java
+++ src/main/java/imageviewer/Main.java
@@ -10,7 +10,7 @@ import java.io.File;
 public class Main {
     
     public static void main(String[] args) {
-        File file = new File("../ImageViewer/images/");
+        File file = new File("../ImageViewer-master/images/");
         
         FileImageLoader loader = new FileImageLoader(file);
         Image image = loader.load();
