7a9ee21beb8a80b6588bcdc36ef7ee2e269298d7
AiramLemes <90828450+AiramLemes@users.noreply.github.com>
2023-01-21 19:24:19 +0000

    Reestructuración de los paquetes

--- src/main/java/imageviewer/Main.java
+++ src/main/java/imageviewer/Main.java
@@ -10,7 +10,7 @@ import java.io.File;
 public class Main {
     
     public static void main(String[] args) {
-        File file = new File("../ImageViewer-master/images/");
+        File file = new File("./images/");
         
         FileImageLoader loader = new FileImageLoader(file);
         Image image = loader.load();
